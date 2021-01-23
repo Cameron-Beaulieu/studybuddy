94b2c702420b823d963976c4457370d8db668ec6
Marc Kevin Quijalvo <33074023+mkevinq@users.noreply.github.com>
2021-01-22 23:01:53 -0500

    add unfinished camera code

--- client/src/components/Camera.js
+++ client/src/components/Camera.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Camera.css';
 
+const coco = require('@tensorflow-models/coco-ssd');
+
 class Camera extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            detectedObjects : []
+        }
+        this.divRef = React.createRef();
         this.cameraRef = React.createRef();
+        this.model = undefined;
     }
 
     componentDidMount() {
@@ -12,9 +19,28 @@ class Camera extends React.Component {
         .then(stream => {
             this.cameraRef.current.srcObject = stream;
             this.cameraRef.current.onloadedmetadata = () => { this.cameraRef.current.play() };
+
+            coco.load()
+            .then((loaded) => {
+                this.model = loaded;
+                setInterval(this.detectObjects.bind(this), 500)
+            })
         })
         .catch(err => {
             console.log("Camera permissions denied");
+        })
+    }
+
+    detectObjects() {
+        this.model.detect(this.cameraRef.current, 20, 0.1)
+        .then((predictions) => {
+            let children = []
+
+            for (let n = 0; n < predictions.length; n++) {
+               children.push(predictions[n])
+            }
+
+            this.setState({ detectedObjects: children })
         })
     }
 
@@ -24,7 +50,7 @@ class Camera extends React.Component {
 
     render() {
         return (
-            <div>
+            <div ref={this.divRef} className="camView">
                 <video ref={this.cameraRef} autoPlay="true" />
             </div>
         )
