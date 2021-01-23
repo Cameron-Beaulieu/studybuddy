1926a85e1c9afeadfb96ff899c0d79d245d9a040
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2021-01-22 22:48:46 -0500

    added state changes for homepage

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,35 +1,51 @@
 import './Home.css';
 import blobs from '../assets/blobs.png';
 import title from '../assets/name.png';
-//import './screens.css';
+import { useEffect, useState } from 'react';
 
 function Home() {
 
-    function getStartButtons(){
+    const [choiceLevel, setChoiceLevel] = useState(1);  // lvl 1 = choosing time, lvl 2 = choosing schedule
+
+    // display button arrays depending on choice level
+    useEffect(() =>{
+        document.getElementById('choice-' + choiceLevel).style.display = 'flex';
+        if (choiceLevel === 1){
+            document.getElementById('choice-2').style.display = 'none';
+        }else if (choiceLevel === 2){
+            document.getElementById('choice-1').style.display = 'none';
+        }
+    });
+
+    // make buttons for choice level 1
+    function getTimeButtons(){
         const buttonList = ['1 hour', '2 hours', '4 hours', '6 hours', 'custom'];
-        const buttons = buttonList.map((button) => createButton(button));
+        const buttons = buttonList.map((button) => {
+            return (<button key={button} onClick={() => setChoiceLevel(2)}>{button}</button>)
+        });
         return buttons;
     }
 
+    // make buttons for choice level 2
     function getMethodButtons(){
         const buttonList = ['pomodoro', '20/20 rule', 'custom'];
-        const buttons = buttonList.map((button) => createButton(button));
+        const buttons = buttonList.map((button) => {
+            return (<button key={button}>{button}</button>)
+        });
         return buttons;
-    }
-
-    function createButton(name){
-        return (
-            <button key={name}>{name}</button>
-        );
     }
 
     return (
         <div className="home">
-            <img id="blobs" src={blobs}></img>
-            <img id="title" src={title}></img>
+            <img id="blobs" src={blobs} alt="studybuddy's logo"></img>
+            <img id="title" src={title} alt="welcome to studybuddy"></img>
             <h2 className="subtitle">START WORK SESSION</h2>
-            <div className="button-array">
-                {getStartButtons()}
+            <div id="choice-1" className="button-array">
+                {getTimeButtons()}
+            </div>
+            <div id="choice-2" className="button-array">
+                <button key="restart" onClick={() => setChoiceLevel(1)}>restart</button>
+                {getMethodButtons()}
             </div>
         </div>
     );
