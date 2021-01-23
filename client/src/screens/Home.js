53a6719bdf3455c485a4c6fbb2414a7ef6449809
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2021-01-22 23:36:21 -0500

    added more functionality for homepage

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -6,6 +6,8 @@ import { useEffect, useState } from 'react';
 function Home() {
 
     const [choiceLevel, setChoiceLevel] = useState(1);  // lvl 1 = choosing time, lvl 2 = choosing schedule
+    const [sessionTime, setSessionTime] = useState(0); // how long session is
+    const [schedule, setSchedule] = useState(''); // schedule choice
 
     // display button arrays depending on choice level
     useEffect(() =>{
@@ -14,6 +16,7 @@ function Home() {
             document.getElementById('choice-2').style.display = 'none';
             document.querySelector('.subtitle').innerHTML = 'START WORK SESSION';
         }else if (choiceLevel === 2){
+            console.log('chosen time: ' + sessionTime);
             document.getElementById('choice-1').style.display = 'none';
             document.querySelector('.subtitle').innerHTML = 'BREAK SCHEDULING METHOD';
         }
@@ -21,20 +24,41 @@ function Home() {
 
     // make buttons for choice level 1
     function getTimeButtons(){
-        const buttonList = ['1 hour', '2 hours', '4 hours', '6 hours', 'custom'];
+        const buttonList = [1, 2, 4, 6];
         const buttons = buttonList.map((button) => {
-            return (<button key={button} onClick={() => setChoiceLevel(2)}>{button}</button>)
+            return (<button key={button} onClick={() => {setSessionTime(button); setChoiceLevel(2);}}>{button} hours</button>)
         });
         return buttons;
     }
 
     // make buttons for choice level 2
     function getMethodButtons(){
-        const buttonList = ['pomodoro', '20/20 rule', 'custom'];
+        const buttonList = [
+            {
+                name: 'pomodoro',
+                description: 'For every 25 minutes, take a break. For each break, if you have taken at least 4 breaks, take a break for 15-30 minutes. Otherwise, take a break for 3-5 minutes.'
+            },
+            {
+                name: '20/20 rule',
+                description: 'For every 20 minutes of work, take 20 seconds to look off your screen.'
+            }
+        ]
+        //['pomodoro', '20/20 rule'];
         const buttons = buttonList.map((button) => {
-            return (<button key={button}>{button}</button>)
+            return (<button key={button.name} title={button.description} onClick={() => {setSchedule(button.name);}}>{button.name}</button>)
         });
         return buttons;
+    }
+
+    // choose custom time
+    function chooseCustomTime(){
+        // make like a popup
+        setSessionTime(0); // arbitrary; delete later
+    }
+
+    function chooseCustomSchedule(){
+        // make popup
+        setSchedule(''); // arbitrary; delete later
     }
 
     return (
@@ -44,10 +68,12 @@ function Home() {
             <h2 className="subtitle">START WORK SESSION</h2>
             <div id="choice-1" className="button-array">
                 {getTimeButtons()}
+                <button key="custom" onClick={chooseCustomTime}>custom</button>
             </div>
             <div id="choice-2" className="button-array">
                 <button key="restart" onClick={() => setChoiceLevel(1)}>restart</button>
                 {getMethodButtons()}
+                <button key="custom" onClick={chooseCustomSchedule}>custom</button>
             </div>
         </div>
     );
