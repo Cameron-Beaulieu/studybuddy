00ba519a558c199221aa8ca264ca852d765e5642
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2021-01-23 16:18:25 -0500

    stylized stats screen

--- client/src/screens/Stats.js
+++ client/src/screens/Stats.js
@@ -1,6 +1,6 @@
 import './Stats.css';
-import header from '../assets/name-w-blobs.png';
-import { PieChart } from 'react-minimal-pie-chart'
+import studybuddy from '../assets/name-w-blobs.png';
+import { PieChart } from 'react-minimal-pie-chart';
 
 function Stats({ productive, slack, breakTime, overBreak, sips, postureChecks, sessionTimeMinutes }) {
 
@@ -16,14 +16,15 @@ function Stats({ productive, slack, breakTime, overBreak, sips, postureChecks, s
     }
 
     function displayTitle() {
-        const month = new Date().getMonth();
-        const day = new Date().getDate();
-        const year = new Date().getYear() + 1900;
+        const today = new Date();
+        const month = today.getMonth();
+        const day = today.getDate();
+        const year = today.getFullYear();
         const months = ["january", "february", "march", "april", "may", "june",
             "july", "august", "september", "october", "november", "december"];
         const selectedMonthName = months[month];
         return (
-            <h1 id="title">stats for session on {selectedMonthName} {day}, {year} </h1>
+            <h2 id="sessionTitle">stats for session on {selectedMonthName} {day}, {year} </h2>
         )
     }
 
@@ -33,14 +34,15 @@ function Stats({ productive, slack, breakTime, overBreak, sips, postureChecks, s
         const ratioBreak = breakTime / (productive + slack + breakTime + overBreak);
         const ratioOver = overBreak / (productive + slack + breakTime + overBreak);
         return (
-            <h1 id="chartInfo">{Math.floor(ratioProductive * sessionTimeMinutes)} minutes productive, {Math.floor(ratioSlack * sessionTimeMinutes)} minutes unproductive, {Math.floor(ratioBreak * sessionTimeMinutes)} minutes on break, {Math.floor(ratioOver * sessionTimeMinutes)} minutes of extra break taken</h1>
+            <h2 id="chartInfo">{Math.floor(ratioProductive * sessionTimeMinutes)} minutes productive, {Math.floor(ratioSlack * sessionTimeMinutes)} minutes unproductive, {Math.floor(ratioBreak * sessionTimeMinutes)} minutes on break, {Math.floor(ratioOver * sessionTimeMinutes)} minutes of extra break taken</h2>
         )
     }
     function displayStats() {
         return (
             <div id="stats">
-                <h1 class="stat">{sips} drinks of water taken</h1>
-                <h1 class="stat">your posture was bad {postureChecks} times</h1>
+                {displayChartInfo()}
+                <h2 class="stat">{sips} drinks of water taken</h2>
+                <h2 class="stat">your posture was bad {postureChecks} times</h2>
             </div>
         )
 
@@ -51,20 +53,23 @@ function Stats({ productive, slack, breakTime, overBreak, sips, postureChecks, s
         const ratioBreak = breakTime / (productive + slack + breakTime + overBreak);
         const ratioOver = overBreak / (productive + slack + breakTime + overBreak);
         const data = [
-            { title: `Productive Time: ${Math.floor(ratioProductive * sessionTimeMinutes)} minutes`, key: 'Productive Time', value: productive, color: '#00A9B4' },
-            { title: `Wasted Time: ${Math.floor(ratioSlack * sessionTimeMinutes)} minutes`, key: 'Wasted Time', value: slack, color: '#AAFAFF' },
-            { title: `Break Time: ${Math.floor(ratioBreak * sessionTimeMinutes)} minutes`, key: 'Break Time', value: breakTime, color: '#00747F' },
-            { title: `Extra Break Time: ${Math.floor(ratioOver * sessionTimeMinutes)} minutes`, key: 'Extra Break Time', value: overBreak, color: '#9D669B' }
+            { title: `productive time: ${Math.floor(ratioProductive * sessionTimeMinutes)} minutes`, key: 'productive time', value: productive, color: '#00A9B4' },
+            { title: `wasted time: ${Math.floor(ratioSlack * sessionTimeMinutes)} minutes`, key: 'wasted time', value: slack, color: '#AAFAFF' },
+            { title: `break time: ${Math.floor(ratioBreak * sessionTimeMinutes)} minutes`, key: 'break time', value: breakTime, color: '#00747F' },
+            { title: `extra break time: ${Math.floor(ratioOver * sessionTimeMinutes)} minutes`, key: 'extra break time', value: overBreak, color: '#9D669B' }
         ]
         return data;
     }
     return (
         <div id="screen">
-            <img id="name-w-blobs" src={header} alt="Study Buddy"></img>
+             <div className="container">
+                <img id="logo" src={studybuddy} alt="studybuddy's logo"></img>
+                <button id="newSession" className="stats-btn">start new session</button>
+            </div>
             <div id="innerSection">
                 {displayTitle()}
                 <div id="pieChart">{displayPieChart()}</div>
-                {displayChartInfo()}
+                <h3>here's how you did:</h3>
                 {displayStats()}
                 
             </div>
