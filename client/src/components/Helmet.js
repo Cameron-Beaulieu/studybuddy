4c5a192032a993766885d3c9c65e09366e6a3b61
Cameron Beaulieu <19cgb@queensu.ca>
2021-01-23 15:43:48 -0500

    Added react helmet

---
+++ client/src/components/Helmet.js
@@ -0,0 +1,12 @@
+import { Helmet } from 'react-helmet';
+
+function Head(){
+    return(
+        <Helmet>
+            <title>studybuddy</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        </Helmet>
+        
+    )
+}
+export default Head;
