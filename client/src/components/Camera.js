262630edbe23756598f7ab7a7075c2a2bddf5633
Marc Kevin Quijalvo <33074023+mkevinq@users.noreply.github.com>
2021-01-22 21:09:20 -0500

    add camera

--- client/src/components/Camera.js
+++ client/src/components/Camera.js
@@ -1,9 +1,34 @@
+import React from 'react';
 import './Camera.css';
 
-function Camera() {
-    return (
+class Camera extends React.Component {
+    constructor(props) {
+        super(props);
+        this.cameraRef = React.createRef();
+    }
 
-    )
+    componentDidMount() {
+        navigator.mediaDevices.getUserMedia({ video: { width: 1280, height: 720 }})
+        .then(stream => {
+            this.cameraRef.current.srcObject = stream;
+            this.cameraRef.current.onloadedmetadata = () => { this.cameraRef.current.play() };
+        })
+        .catch(err => {
+            console.log("Camera permissions denied");
+        })
+    }
+
+    videoError() {
+        console.log("Camera permissions are needed for this app to work");
+    }
+
+    render() {
+        return (
+            <div>
+                <video ref={this.cameraRef} autoPlay="true" />
+            </div>
+        )
+    }
 }
 
-export default Camera
+export default Camera;
